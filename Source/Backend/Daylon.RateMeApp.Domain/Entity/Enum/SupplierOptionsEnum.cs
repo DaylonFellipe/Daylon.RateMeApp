b80353fb890b0c2641dae70520a1d41d7d7e3185
daylon <daylonfellipe3001@outlook.com>
2026-03-04 18:58:41 +0000

    ADD - Added Product and Categories Entities Enums

---
+++ Source/Backend/Daylon.RateMeApp.Domain/Entity/Enum/SupplierOptionsEnum.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Daylon.RateMeApp.Domain.Entity.Enum
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+
+    public enum SupplierOptionsEnum
+    {
+        Indefinite = 0,
+        Continente = 1,
+        PingoDoce = 2,
+        Auchan = 3,
+        Lidl = 4,
+        Minipreco = 5,
+        Intermarche = 6,
+        Aldi = 7,
+        Mercadona = 8
+    }
+}
---
+++ Source/Backend/Daylon.RateMeApp.Domain/Entity/Product.cs
@@ -0,0 +1,30 @@
+﻿
+using Daylon.RateMeApp.Domain.Entity.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace Daylon.RateMeApp.Domain.Entity
+{
+    public class Product
+    {
+        // Product Properts
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double? Price { get; set; }
+
+        // Rating
+        [Range(0, 5, ErrorMessage = "Rating must be between 0 and 5.")]
+        public double? Rating { get; set; }
+
+        // Image
+        public string? ImageUrl { get; set; }
+
+        // Category/SubCategory
+        public Guid CategoryId { get; set; }
+        public Guid SubCategoryId { get; set; }
+
+        // Supplier
+        public SupplierOptionsEnum? SupplierOption { get; set; }
+        public string? SupplierPersonalizedName { get; set; }
+    }
+}
