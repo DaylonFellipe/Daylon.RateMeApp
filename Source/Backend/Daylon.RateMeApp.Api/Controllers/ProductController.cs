182ebcf32b6d65c0b3aa836a16edb8a4661ebee7
daylon <daylonfellipe3001@outlook.com>
2026-03-05 20:07:56 +0000

    ADD - Added DbContext

---
+++ Source/Backend/Daylon.RateMeApp.Api/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Daylon.RateMeApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+
+
+
+
+        // POST
+        // bombar dps, completar
+
+        //public async Task<IActionResult> CreteProductAsync([FromBody] RequestCreateProductJson request) //criar request json no shared commmunication
+        //{
+        //    if (!ModelState.IsValid)
+        //        return BadRequest(ModelState);
+
+        //    var product = await _productService.CreateProductAsync(request); // criar service e interface
+
+        //    return Created("", product);
+        //}
+    }
+}
+
+//   /ᐠ - ˕ -マ D A Y L O N
