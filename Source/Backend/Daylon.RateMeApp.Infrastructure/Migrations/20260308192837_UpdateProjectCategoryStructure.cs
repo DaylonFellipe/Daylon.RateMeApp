23b4118868fa76681c071d121b5eea3c23ad75b5
daylon <daylonfellipe3001@outlook.com>
2026-03-08 20:09:50 +0000

    UPD - Remove Category Entity and Update Database

---
+++ Source/Backend/Daylon.RateMeApp.Infrastructure/Migrations/20260308192837_UpdateProjectCategoryStructure.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Daylon.RateMeApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProjectCategoryStructure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SubCategoryId",
+                table: "Products");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Category",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SubCategory",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SubCategory",
+                table: "Products");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CategoryId",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "SubCategoryId",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
