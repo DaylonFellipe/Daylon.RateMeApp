ccc17a3c1ccc65f99d7662659e61c530cc1d84d3
daylon <daylonfellipe3001@outlook.com>
2026-03-10 20:12:15 +0000

    ADD - Created Custom Exception Filter

--- Source/Shared/Daylon.RateMeApp.Exceptions/ExceptionBases/ErrorOnValidationException.cs
+++ Source/Shared/Daylon.RateMeApp.Exceptions/ExceptionBases/ErrorOnValidationException.cs
@@ -1,6 +1,8 @@
-﻿namespace Daylon.RateMeApp.Exceptions.ExceptionBases
+﻿using Daylon.RateMeApp.Communication.Responses.Error;
+
+namespace Daylon.RateMeApp.Exceptions.ExceptionBases
 {
-    public class ErrorOnValidationException(List<string> validationErrors) : Exception
+    public class ErrorOnValidationException(List<string> validationErrors) : RateMeAppException
     {
         public List<string> ValidationErrorsMessages = validationErrors;
     }
