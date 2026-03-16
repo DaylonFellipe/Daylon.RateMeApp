03f0c3b9ca25c8da509d3f524486c73419354158
daylon <daylonfellipe3001@outlook.com>
2026-03-16 18:14:36 +0000

    ADD - Added Product UseCase Tests

---
+++ Tests/CommonTestUtilities/Repositories/Product/ProductRepositoryInMemory.cs
@@ -0,0 +1,50 @@
+﻿using Daylon.RateMeApp.Domain.Interfaces.Repositories;
+
+namespace CommonTestUtilities.Repositories.Product
+{
+    public class ProductRepositoryInMemory : IProductRepository
+    {
+        private readonly List<Daylon.RateMeApp.Domain.Entity.Product> _products = [];
+
+        // Get
+        public Task<IEnumerable<Daylon.RateMeApp.Domain.Entity.Product>> GetAllProductsAsync()
+        {
+            var products = _products.AsEnumerable();
+            return Task.FromResult(products);
+        }
+
+        public Task<Daylon.RateMeApp.Domain.Entity.Product?> GetProductByIdAsync(Guid id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            return Task.FromResult(product);
+        }
+
+        // Post
+        public Task CreateProductAsync(Daylon.RateMeApp.Domain.Entity.Product product)
+        {
+            _products.Add(product);
+            return Task.CompletedTask;
+        }
+
+        // Put
+        public Task UpdateProductAsync(Daylon.RateMeApp.Domain.Entity.Product product)
+        {
+            var existingProduct = GetProductByIdAsync(product.Id).Result;
+
+            if (existingProduct != null)
+            {
+                _products.Remove(existingProduct);
+                _products.Add(product);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        // Delete
+        public Task<bool> DeleteProductAsync(Guid id)
+        {
+            _products.RemoveAll(p => p.Id == id);
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ Tests/UseCases.Test/Product/ProductUseCaseTest.cs
@@ -0,0 +1,71 @@
+﻿using CommonTestUtilities.Repositories.Product;
+using CommonTestUtilities.Requests.Product;
+using Daylon.RateMeApp.Application.UseCases.Product;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+
+// The request properties are tested in Validator.Test - empty fields, invalid requests...
+
+namespace UseCases.Test.Product
+{
+    public class ProductUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            var request = RequestCreateProductJsonBuilder.Build();
+
+            var useCase = CreateUseCase();
+
+            var result = await useCase.ExecuteCreateProductAsync(request);
+
+            result.Should().NotBeNull();
+            result.Name.Should().Be(request.Name);
+            result.Description.Should().Be(request.Description);
+            result.Price.Should().Be(request.Price);
+            result.Rating.Should().Be(request.Rating);
+            result.ImageUrl.Should().Be(request.ImageUrl);
+            result.Category.Should().Be(request.Category);
+            result.SubCategory.Should().Be(request.SubCategory);
+            result.SupplierOption.Should().Be(request.SupplierOption);
+            result.SupplierPersonalizedName.Should().Be(request.SupplierPersonalizedName);
+        }
+
+        [Fact]
+        public async Task Success_Update()
+        {
+            var useCase = CreateUseCase();
+
+            // Create a product
+            var requestProduct = RequestCreateProductJsonBuilder.Build();
+            var productResult = await useCase.ExecuteCreateProductAsync(requestProduct);
+
+            // Update the product
+            var request = RequestUpdateProductJsonBuilder.Build(productResult.Id);
+            var result = await useCase.ExecuteUpdateProductAsync(request);
+
+            result.Should().NotBeNull(); 
+            productResult.Id.Should().Be(result.Id);
+
+            result.Name.Should().Be(request.Name);
+            result.Description.Should().Be(request.Description);
+            result.Price.Should().Be(request.Price);
+            result.Rating.Should().Be(request.Rating);
+            result.ImageUrl.Should().Be(request.ImageUrl);
+            result.Category.Should().Be(request.Category);
+            result.SubCategory.Should().Be(request.SubCategory);
+            result.SupplierOption.Should().Be(request.SupplierOption);
+            result.SupplierPersonalizedName.Should().Be(request.SupplierPersonalizedName);
+        }
+
+        // Auxiliary methods
+        private static ProductUseCase CreateUseCase()
+        {
+            _ = new ConfigurationBuilder().Build();
+
+            var productRepositoryBuilder = new ProductRepositoryInMemory();
+
+            return new ProductUseCase(productRepositoryBuilder);
+        }
+    }
+}
